640c8dc889170dd4801bb392f4210af75656d748
Josh Sparks <joshsparks@Joshs-MacBook-Pro.local>
2019-10-25 10:47:56 -0600

    worked on pairController and PairListTableVC

---
+++ Pair/Pair/Controllers/Model Controllers/PairController.swift
@@ -0,0 +1,89 @@
+//
+//  PairController.swift
+//  Pair
+//
+//  Created by Josh Sparks on 10/25/19.
+//  Copyright © 2019 Josh Sparks. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+class PairController {
+    
+    
+    static let shared = PairController()
+    let publicDB = CKContainer.default().publicCloudDatabase
+    var pairs: [Pair] = []
+    
+    
+    
+    func addName(with name: String, completion: @escaping (_ success: Bool) -> (Void)) {
+        let newName = Pair(name: name)
+        let nameRecord = CKRecord(name: newName)
+        
+        publicDB.save(nameRecord) { (record, error) in
+            if let error = error{
+                print("\(error.localizedDescription) \(error) in function: \(#function)")
+                completion(false)
+                return
+            }
+            
+            guard let record = record,
+                let savedName = Pair(ckRecord: record) else { completion(false) ; return }
+            self.pairs.append(savedName)
+            print("Saved name successfully")
+            completion(true)
+        }
+        
+    }
+    
+    func removeName(with name: Pair) {
+        guard let nameIndex = pairs.firstIndex(of: name) else { return }
+        pairs.remove(at: nameIndex)
+    }
+    
+    func updateName(_ name: Pair, completion: @escaping (_ success: Bool) -> (Void)) {
+        let recordToUpdate = CKRecord(name: name)
+        let operation = CKModifyRecordsOperation(recordsToSave: [recordToUpdate], recordIDsToDelete: nil)
+        
+        operation.savePolicy = .changedKeys
+        operation.qualityOfService = .userInteractive
+        operation.modifyRecordsCompletionBlock = { records, _, error in
+            if let error = error {
+                print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                completion(false)
+                return
+            }
+            guard recordToUpdate == records?.first else {
+                print("Unexpected record was updated")
+                completion(false)
+                return
+            }
+            
+            print("Updated \(recordToUpdate.recordID) successfully in CloudKit")
+            completion(true)
+        }
+        publicDB.add(operation)
+    }
+    
+    func fetchName(completion: @escaping (_ success: Bool) -> (Void)) {
+           let predicate = NSPredicate(value: true)
+             
+             let query = CKQuery(recordType: PairStrings.recordTypeKey, predicate: predicate)
+             
+             publicDB.perform(query, inZoneWith: nil) { (foundRecords, error) in
+                 if let error = error {
+                     print("Error in \(#function) : \(error.localizedDescription) \n---\n \(error)")
+                     completion(false)
+                     return
+                 }
+                 
+                 guard let records = foundRecords else { completion(false) ; return }
+                 let pairs = records.compactMap( { Pair(ckRecord: $0) } )
+                 self.pairs = pairs
+                 completion(true)
+             }
+         }
+    
+} // end of class
---
+++ Pair/Pair/Models/Pair.swift
@@ -0,0 +1,49 @@
+//
+//  Pair.swift
+//  Pair
+//
+//  Created by Josh Sparks on 10/25/19.
+//  Copyright © 2019 Josh Sparks. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+struct PairStrings {
+    static let recordTypeKey = "Pair"
+    fileprivate static let nameKey = "name"
+}
+
+class Pair {
+    var name: String
+    var ckRecordID: CKRecord.ID
+    
+    init(name: String, ckRecordID: CKRecord.ID = CKRecord.ID(recordName: UUID().uuidString)) {
+        self.name = name
+        self.ckRecordID = ckRecordID
+    }
+}
+
+extension Pair {
+    convenience init? (ckRecord: CKRecord) {
+        guard let name = ckRecord[PairStrings.nameKey] as? String else { return nil }
+        
+        self.init(name: name, ckRecordID: ckRecord.recordID)
+    }
+}
+
+extension Pair: Equatable {
+    static func == (lhs: Pair, rhs: Pair) -> Bool {
+        return lhs.ckRecordID == rhs.ckRecordID
+    }
+}
+
+extension CKRecord {
+    convenience init(name: Pair) {
+        self.init(recordType: PairStrings.recordTypeKey, recordID: name.ckRecordID)
+        
+        self.setValuesForKeys([
+            PairStrings.nameKey : name.name
+        ])
+    }
+}
